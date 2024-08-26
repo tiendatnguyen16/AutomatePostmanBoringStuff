e2f5853d1782560579b9af89706e1369838373ca
Dat Nguyen <80873477+tiendatnguyen16@users.noreply.github.com>
2024-08-26 13:39:23 +0700

    Update debugging_Dat.js

--- debugging_Dat.js
+++ debugging_Dat.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const API_KEY = 'PMAK-668b482c89b08d00013a0780-38afe2fd58843dbfa1219c1bc75575ce69'; // Replace with your actual API key
+const API_KEY = '<your API Key>'; // Replace with your actual API key
 
 const headers = {
   'X-Api-Key': API_KEY,
