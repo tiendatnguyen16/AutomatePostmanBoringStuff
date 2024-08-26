84c4ba903b43ce2de364eaeed99d29385ea17552
Dat Nguyen <80873477+tiendatnguyen16@users.noreply.github.com>
2024-08-26 13:38:19 +0700

    Update forkCollections.js

--- forkCollections.js
+++ forkCollections.js
@@ -13,9 +13,9 @@ const destinationWorkspaceId = '37d13948-c437-4443-8fda-47e70266908c';
 
 const axios = require('axios');
 
-const API_KEY = 'PMAK-668b482c89b08d00013a0780-38afe2fd58843dbfa1219c1bc75575ce69'; // Replace with your actual API key
-const SOURCE_WORKSPACE_ID = 'f1c6b0a9-b930-4165-9aa4-f655dd7051b5'; // Replace with your source workspace ID
-const DESTINATION_WORKSPACE_ID = '37d13948-c437-4443-8fda-47e70266908c'; // Replace with your destination workspace ID
+const API_KEY = '<your API Key>'; // Replace with your actual API key
+const SOURCE_WORKSPACE_ID = '<source workspace ID>'; // Replace with your source workspace ID
+const DESTINATION_WORKSPACE_ID = '<destination workspace ID>'; // Replace with your destination workspace ID
 
 const headers = {
   'X-Api-Key': API_KEY,
